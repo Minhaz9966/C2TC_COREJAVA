94df70e49fa89fb3de9704fc3d34557fbed33784
minha <minha@MINHAZ>
2023-10-04 11:19:46 +0530

    day2

---
+++ DayTwo/src/ForEachLoop.java
@@ -0,0 +1,18 @@
+
+public class ForEachLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {10,20,30,40,50};
+		
+		for(int temp :a) {
+			System.out.println(temp);
+			
+		}
+		
+       String s[]= ("Hello","welcome","to","C2CT","program");
+       for(String temp :s)
+    	   System.out.println(temp);
+	}
+
+}
